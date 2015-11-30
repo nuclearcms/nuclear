9e126652b2209d659fcf3e9df103799cdf415b78
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-11-30 19:17:45 +0200

    Added extensions directory

---
+++ extension/Site/Providers/SiteExtensionServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Extension\Site\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class SiteExtensionServiceProvider extends ServiceProvider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
