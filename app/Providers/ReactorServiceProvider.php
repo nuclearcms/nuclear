c80ea5b33b7909593cfe291021d10055cb822828
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-12-30 00:16:15 +0200

    Updated Hierarchy version

--- app/Providers/ReactorServiceProvider.php
+++ app/Providers/ReactorServiceProvider.php
@@ -10,7 +10,7 @@ use Reactor\Nodes\Node;
 
 class ReactorServiceProvider extends ServiceProvider {
 
-    const VERSION = '2.0.13';
+    const VERSION = '2.0.14';
 
     /**
      * Register any application services.
