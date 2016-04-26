3ea189c86559474edc5419387d56dc89d765731f
Hyleeh <egemen@kenarkose.com>
2016-04-27 00:33:39 +0300

    Updated dependencies

--- app/Providers/ReactorServiceProvider.php
+++ app/Providers/ReactorServiceProvider.php
@@ -8,7 +8,7 @@ use Theme;
 
 class ReactorServiceProvider extends ServiceProvider {
 
-    const VERSION = '2.8.0';
+    const VERSION = '2.8.1';
 
     /**
      * Register any application services.
