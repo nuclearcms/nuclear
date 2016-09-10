1912b04f42b53791208ce37de3deab361fb70d12
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2016-09-10 15:27:36 +0300

    Small fix

--- resources/views/reactor/update/index.blade.php
+++ resources/views/reactor/update/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="content-inner">
 
-        @if(!$updater->isNuclearCurrent())
+        @if($updater->isNuclearCurrent())
             <div>
                 <h3 class="content-inner__heading content-inner__heading--success"><i class="icon-status-published"></i> {{ trans('update.nuclear_is_up_to_date') }}</h3>
                 <p class="content-inner__message">{{ trans('update.up_to_date_description', ['version' => nuclear_version()]) }}</p>
