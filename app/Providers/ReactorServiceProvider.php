16cb13f3c48d1372d34e152a84c894b5fc1ef703
Egemen Berker Kızılcan <egemen@kenarkose.com>
2016-10-16 11:51:00 +0300

    Updated dependencies

--- app/Providers/ReactorServiceProvider.php
+++ app/Providers/ReactorServiceProvider.php
@@ -11,7 +11,7 @@ use Reactor\Support\Routing\RouteFilterMaker;
 
 class ReactorServiceProvider extends ServiceProvider {
 
-    const VERSION = '3.0-alpha.9';
+    const VERSION = '3.0-alpha.10';
 
     /**
      * Register any application services.
