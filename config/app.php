74a850e8a848fac3712e2fd131b8b38c4cff4368
Egemen Berker Kızılcan <egemen@umomega.com>
2020-12-07 21:37:27 +0300

    Removed remaining telescope stuff

--- config/app.php
+++ config/app.php
@@ -208,8 +208,7 @@ return [
         // App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
+        App\Providers\RouteServiceProvider::class
 
     ],
 
